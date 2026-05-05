38d4e464a2c32d07ae8f6d557df4ee4d5ceffc9b
AlbertoAIG <alberto.aig@me.com>
2026-05-05 11:19:45 +0200

    Refactor ProwlNotification accessory handling
    Refactor accessory management and notification logic.

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ module.exports = function(homebridge) {
     homebridge.registerPlatform("homebridge-prowl-notification", "ProwlNotification", ProwlNotification, true);
 }
 
-// Platform constructor
-// config may be null
-// api may be null if launched from old homebridge version
 function ProwlNotification(log, config, api) {
     this.log = log;
     this.config = config;
-    this.accessories = [];
+    // CORRECCIÓN: Inicializar como objeto para usar nombres como llaves
+    this.accessories = {}; 
     this.switches = this.config.switches || [];
     
     if (api) {
@@ -29,115 +27,102 @@ function ProwlNotification(log, config, api) {
     }
 }
 
-// Method to setup accesories from config.json
 ProwlNotification.prototype.didFinishLaunching = function () {
-    // Add or update accessories defined in config.json
-    for (var i in this.switches) this.addAccessory(this.switches[i]);
+    // Añadir o actualizar accesorios
+    for (var i = 0; i < this.switches.length; i++) {
+        this.addAccessory(this.switches[i]);
+    }
     
-    // Remove extra accessories in cache
+    // Eliminar accesorios antiguos que ya no están en el config
     for (var name in this.accessories) {
-      var accessory = this.accessories[name];
-      if (!accessory.reachable) this.removeAccessory(accessory);
+        var accessory = this.accessories[name];
+        if (!accessory.reachable) this.removeAccessory(accessory);
     }
 }
 
-
-// Function invoked when homebridge tries to restore cached accessory.
-// Developer can configure accessory at here (like setup event handler).
-// Update current value.
 ProwlNotification.prototype.configureAccessory = function(accessory) {
     this.log("Configure cached accessory:", accessory.displayName);
+    var name = accessory.context.name || accessory.displayName;
     
     this.setService(accessory);
-    this.accessories[accessory.context.name] = accessory;
+    this.accessories[name] = accessory;
 }
 
-
-
-
-// we get accessory data from config.json
 ProwlNotification.prototype.addAccessory = function(data) {
+    if (!data.name) {
+        this.log.warn("Accesorio omitido: No tiene nombre definido en el config.");
+        return;
+    }
+
     var accessory = this.accessories[data.name];
     
     if (!accessory) {
         this.log("Add new accessory:", data.name);
-        // Setup accessory as SWITCH (8) category.
         var uuid = UUIDGen.generate(data.name);
-        accessory = new Accessory(data.name, uuid, 8);
-        // Setup HomeKit switch service
+        accessory = new Accessory(data.name, uuid, 8); // 8 = Switch
+        
         accessory.addService(Service.Switch, data.name);
-        // New accessory is always reachable
-        accessory.reachable = true;
-        // Setup listeners for different switch events
         this.setService(accessory);
-        // Register new accessory in HomeKit
+        
         this.api.registerPlatformAccessories("homebridge-prowl-notification", "ProwlNotification", [accessory]);
+        this.accessories[data.name] = accessory;
     }
-    /*
-     name: name in Homekit
-     subject: subject of  push notification, default is room name
-     message: notification text to send
-     priority: -2 to 2 priority, default 0
-     */
+
+    // Asegurar que context existe antes de asignar
+    if (!accessory.context) accessory.context = {};
     
     accessory.context.name = data.name;
-    accessory.context.priority = (data.priority == undefined) ? 0 : data.priority;
+    accessory.context.priority = (data.priority === undefined) ? 0 : data.priority;
 
-    // getInitState
-    var manufacturer = accessory.context.manufacturer || "Simone Karin Lehmann";
-    var model = accessory.context.model || "Prowl Notification Switch";
-    var serial = accessory.context.serial || package.version;
+    var manufacturer = "Simone Karin Lehmann";
+    var model = "Prowl Notification Switch";
+    var serial = package.version || "1.0.0";
     
-    // Update HomeKit accessory information
     accessory.getService(Service.AccessoryInformation)
-    .setCharacteristic(Characteristic.Manufacturer, manufacturer)
-    .setCharacteristic(Characteristic.Model, model)
-    .setCharacteristic(Characteristic.SerialNumber, serial);
+        .setCharacteristic(Characteristic.Manufacturer, manufacturer)
+        .setCharacteristic(Characteristic.Model, model)
+        .setCharacteristic(Characteristic.SerialNumber, serial);
     
-    // Store accessory in cache
-    this.accessories[data.name] = accessory;
-    // Configured accessory is reachable
     accessory.updateReachability(true);
 }
 
-
-// Method to setup listeners for different events
 ProwlNotification.prototype.setService = function (accessory) {
-    accessory.getService(Service.Switch)
-    .getCharacteristic(Characteristic.On)
-    .on('get', this.getState.bind(this, accessory.context))
-    .on('set', this.setState.bind(this, accessory.context));
+    var service = accessory.getService(Service.Switch);
+    if (service) {
+        service.getCharacteristic(Characteristic.On)
+            .on('get', this.getState.bind(this, accessory.context))
+            .on('set', this.setState.bind(this, accessory.context));
+    }
     
     accessory.on('identify', this.identify.bind(this, accessory.context));
 }
 
-// Method to handle identify request
 ProwlNotification.prototype.identify = function (thisSwitch, paired, callback) {
     this.log(thisSwitch.name + " identify requested!");
     callback();
 }
 
-// Method to remove accessories from HomeKit
 ProwlNotification.prototype.removeAccessory = function (accessory) {
-  if (accessory) {
-    var name = accessory.context.name;
-    this.api.unregisterPlatformAccessories("homebridge-prowl-notification", "ProwlNotification", [accessory]);
-    delete this.accessories[name];
-  }
+    if (accessory) {
+        var name = accessory.context.name;
+        this.api.unregisterPlatformAccessories("homebridge-prowl-notification", "ProwlNotification", [accessory]);
+        delete this.accessories[name];
+    }
 }
 
 ProwlNotification.prototype.getState = function (thisSwitch, callback) {
-    var currentState = false
-    callback(null, currentState);
+    callback(null, false);
 }
 
 ProwlNotification.prototype.setState = function (thisSwitch, state, callback) {
     var self = this;
 
-    if (state == true) {
+    if (state === true) {
         setTimeout(function () {
-            self.accessories[thisSwitch.name].getService(Service.Switch)
-                .setCharacteristic(Characteristic.On, !state);
+            var acc = self.accessories[thisSwitch.name];
+            if (acc) {
+                acc.getService(Service.Switch).setCharacteristic(Characteristic.On, false);
+            }
         }, 3000);
         this.sendNotification(thisSwitch);
     }
@@ -147,51 +132,47 @@ ProwlNotification.prototype.setState = function (thisSwitch, state, callback) {
 ProwlNotification.prototype.sendNotification = function(thisSwitch) {
     this.log.debug("send notification from " + thisSwitch.name);
 
-    var switches;
-    var defaultMessage;
-    var subject;
-    var message;
-    var globalConfig = JSON.parse(fs.readFileSync(this.api.user.configPath()));
+    var defaultMessage = (this.config.defaultmsg === undefined) ? "%s has been triggered." : this.config.defaultmsg;
+    var subject = thisSwitch.name;
+    var message = format(defaultMessage, subject);
 
-    globalConfig.platforms.forEach(p => {
-        if (p.platform === "ProwlNotification") {
-            defaultMessage = (p.defaultmsg == undefined) ? "% has been triggered." : p.defaultmsg;
-            switches = p.switches;
+    // Buscar si hay sobreescritura específica en el config de este switch
+    if (this.switches) {
+        for (var i = 0; i < this.switches.length; i++) {
+            if (this.switches[i].name === thisSwitch.name) {
+                if (this.switches[i].subject) subject = this.switches[i].subject;
+                if (this.switches[i].message) message = this.switches[i].message;
+                break;
+            }
         }
-    });
-    switches.forEach(s => {
-        if (s.name === thisSwitch.name) {
-            subject = (s.subject == undefined) ? thisSwitch.name : s.subject;
-            message = (s.message == undefined) ? format(defaultMessage, subject) : s.message;
-        }
-    });
+    }
 
-    var data = querystring.stringify({ apikey : this.config.apikey,
-                                     application : subject,
-                                     description : message,
-                                     priority : thisSwitch.priority
-                                     })
+    var data = querystring.stringify({ 
+        apikey : this.config.apikey,
+        application : subject,
+        description : message,
+        priority : thisSwitch.priority
+    });
     
     var options = {
-    hostname: 'api.prowlapp.com',
-    port: 443,
-    path: '/publicapi/add',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': data.length
-    }
+        hostname: 'api.prowlapp.com',
+        port: 443,
+        path: '/publicapi/add',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': data.length
+        }
     };
     
     var req = https.request(options, res => {
-                            this.log.debug(`sendNotification statusCode: ${res.statusCode}`)
-                            })
+        this.log.debug(`sendNotification statusCode: ${res.statusCode}`);
+    });
     
     req.on('error', error => {
-           this.log.error("sendNotification: " + error)
-           })
+        this.log.error("sendNotification: " + error);
+    });
     
-    req.write(data)
-    req.end()
-    
+    req.write(data);
+    req.end();
 }
